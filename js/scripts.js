b948f95df72f4cf10703ece43b0722adf4616e43
Stewart ilondanga <stilondanga@gmail.com>
2018-10-14 13:02:14 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,9 @@ var navigate = (function() {
     $('.' + dataName).toggle();
   });
 })();
-/*
+
 // include variables for the cards: the suit, value and color
-const cardSuit = ["♦","♥","♣","♠"];
+/*const cardSuit = ["♦","♥","♣","♠"];
 const cardValue = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 const cardColor = ["#DD5964", "#483F50"];
 
